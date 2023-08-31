6224e554fd89774de14d8ac1b193dae397219fa3
mika1407 <koneautomaatio@hotmail.com>
2023-08-31 17:47:23 +0300

    eka valmis

--- quiz_game.py
+++ quiz_game.py
@@ -5,8 +5,36 @@ playinig = input("Do you want to play? ")
 if playinig.lower() != "yes":
     quit()
 
-print("Ok! lets play :)")  
+print("Ok! lets play :)")
+score = 0  
 
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What is the capital of Finland? ")
+if answer.lower() == "helsinki":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What year was Tampere founded? ")
+if answer.lower() == "1779":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " out of 4 points")
+print("You got " + str((score/4)*100) + "%.")
